b8f3af3dd75e9244b3502d7ac16b16933f1730ca
veehighsmith <veehighsmith@gmail.com>
2021-02-13 10:05:40 -0600

    new build

---
+++ build/precache-manifest.78a341b70e6d504bca07fa32c3f3587f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ba478c7af24cd86083e9bb819c8a98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70fe64d013b3ff04c515",
+    "url": "/portfolio/static/css/main.46a0fbfb.chunk.css"
+  },
+  {
+    "revision": "205a68fd286036aad29f",
+    "url": "/portfolio/static/js/2.4a2b2070.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/portfolio/static/js/2.4a2b2070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fe64d013b3ff04c515",
+    "url": "/portfolio/static/js/main.cd70f632.chunk.js"
+  },
+  {
+    "revision": "d99f04ae97d3543f6fe6",
+    "url": "/portfolio/static/js/runtime-main.31a18050.js"
+  },
+  {
+    "revision": "50d7d4aa9bc6f2692c5dec55063b7aa3",
+    "url": "/portfolio/static/media/chipotle.50d7d4aa.png"
+  },
+  {
+    "revision": "c3d022debf399e848b0e69f4f4a8802b",
+    "url": "/portfolio/static/media/dish.c3d022de.png"
+  },
+  {
+    "revision": "dd27b2640d2cbe0fe37dfa03dc221d7a",
+    "url": "/portfolio/static/media/hand.dd27b264.png"
+  },
+  {
+    "revision": "edc355181a308f12f61cf6b189b88bd7",
+    "url": "/portfolio/static/media/ibm.edc35518.png"
+  },
+  {
+    "revision": "651305c20469af2ed7490167a47c1548",
+    "url": "/portfolio/static/media/lifetime.651305c2.png"
+  },
+  {
+    "revision": "3f11c22ee0228f7492c56b9bd785871e",
+    "url": "/portfolio/static/media/purina.3f11c22e.png"
+  },
+  {
+    "revision": "794e335c43ffe5ea299a2ed0e12024aa",
+    "url": "/portfolio/static/media/skrillex.794e335c.png"
+  },
+  {
+    "revision": "40811ea17b3d61e6c31591cf575dbac7",
+    "url": "/portfolio/static/media/vee_logo.40811ea1.png"
+  }
+]);
